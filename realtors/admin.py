b592f386da77fe365856f135d92b7f712117575d
drezir <adamostrozlik@yandex.com>
2019-10-09 21:48:34 +0200

    Fetching from database

--- realtors/admin.py
+++ realtors/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Realtor
+
+
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display_links = ('id', 'name')
+    list_per_page = 25
+    search_fields = ('name',)
+
+
 # Register your models here.
+admin.site.register(Realtor, RealtorAdmin)
