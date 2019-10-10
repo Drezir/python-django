ec4d268df594fbf846bc467da361d505e307b565
drezir <adamostrozlik@yandex.com>
2019-10-10 20:29:14 +0200

    Pagination

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from .models import Listing
@@ -5,9 +6,14 @@ from .models import Listing
 
 # Create your views here.
 def index(request):
-    listings = Listing.objects.all()
+    listings = Listing.objects \
+        .order_by('-list_date') \
+        .filter(is_published=True)
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
     context = {
-        'listings': listings
+        'listings': paged_listings
     }
     return render(request, 'listings/listings.html', context)
 
