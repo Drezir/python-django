153733c54074c9c177e6b9b7bf829bb8adbaa9dc
drezir <adamostrozlik@yandex.com>
2019-11-09 10:51:01 +0100

    Login / logout / register statics

---
+++ accounts/views.py
@@ -0,0 +1,27 @@
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.shortcuts import render
+
+
+def register(request):
+    if request.method == 'POST':
+        messages.error(request, 'Testing error message')
+        return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        # login user
+        return
+    else:
+        return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    return redirect('index')
+
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
