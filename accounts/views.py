ae3beef69ddacf16c9ca9616471f442a06ea3ad8
drezir <adamostrozlik@yandex.com>
2019-11-24 18:05:12 +0100

    Contacts

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from django.shortcuts import redirect
 from django.shortcuts import render
 
+from contacts.models import Contact
+
 
 def register(request):
     if request.method == 'POST':
@@ -56,4 +58,8 @@ def logout(request):
 
 
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    user_contacts = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
+    context = {
+        'contacts': user_contacts
+    }
+    return render(request, 'accounts/dashboard.html', context)
--- btre/settings.py
+++ btre/settings.py
@@ -142,3 +142,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+# Email config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'st43312@student.upce.cz'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
